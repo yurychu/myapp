4f37ab978d4392e1dcf69f4fa3665a9aabc1f51f
yurychu <yury_chugunov@mail.ru>
2017-01-29 21:52:55 +0300

    del old start new

--- mybooks/mybooks/wsgi.py
+++ mybooks/mybooks/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for myapp project.
+WSGI config for mybooks project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mybooks.settings")
 
 application = get_wsgi_application()
