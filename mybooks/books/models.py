26fb75889d128120b345e59702e3b4979a7e0297
yurychu <yury_chugunov@mail.ru>
2017-02-05 11:44:45 +0300

    remastering models

--- mybooks/books/models.py
+++ mybooks/books/models.py
@@ -1,19 +1,36 @@
 from django.db import models
 
 
-class Publisher(models.Model):
-    name = models.CharField(max_length=64)
+class Author(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.name
+
+
+class Epos(models.Model):
+    name = models.CharField(max_length=128)
+
 
 class Book(models.Model):
-    title = models.CharField(max_length=128)
-    publisher = models.ForeignKey(Publisher)
+    authors = models.ManyToManyField(Author)
     genre = models.ForeignKey(Genre)
+
+    title = models.CharField(max_length=128)
+    tom = models.CharField(max_length=16)
     pub_year = models.DateField()
     description = models.TextField()
     cover = models.ImageField()
 
+    created = models.DateField(auto_now_add=True)
+    modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
