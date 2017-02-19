f4cdf010cc342f6ca5dc33d10f101ad3eb4ce7b5
yurychu <yury_chugunov@mail.ru>
2017-02-19 23:44:42 +0300

    detail view and template

--- mybooks/books/urls.py
+++ mybooks/books/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.BookListView.as_view(), name='book_list'),
+
+    url(regex=r'^$',
+        view=views.BookListView.as_view(),
+        name='book_list'),
+
+    url(regex=r'^(?P<pk>[0-9]+)/$',
+        view=views.BookDetailView.as_view(),
+        name='book_detail')
 ]
--- mybooks/books/views.py
+++ mybooks/books/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Book
 
 
 class BookListView(ListView):
+    model = Book
+
+
+class BookDetailView(DetailView):
     model = Book
