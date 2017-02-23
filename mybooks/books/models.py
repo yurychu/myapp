ac6e479a0f3ea4a21cb6e1c55c95afa24f6d23ae
yurychu <yury_chugunov@mail.ru>
2017-02-23 23:35:33 +0300

    adding format size model

--- mybooks/books/models.py
+++ mybooks/books/models.py
@@ -29,6 +29,14 @@ class Epos(models.Model):
         return self.name
 
 
+class FormatSize(models.Model):
+    height = models.PositiveIntegerField()
+    width = models.PositiveIntegerField()
+
+    def __str__(self):
+        return '{0} x {1}'.format(self.height, self.width)
+
+
 class Book(models.Model):
     genre = models.ForeignKey(Genre)
     epos = models.ForeignKey(Epos, null=True, blank=True)
@@ -42,6 +50,7 @@ class Book(models.Model):
     description = models.TextField()
     brief_content = models.TextField()
     cover = models.ImageField(blank=True, upload_to='book_covers')
+    format_size = models.ForeignKey(FormatSize, null=True, blank=True)
 
     created = models.DateField(auto_now_add=True)
     modified = models.DateField(auto_now=True)
